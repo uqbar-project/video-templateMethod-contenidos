fdcc8aec774ced222b4c3dac504f6008fd711ef1
Fernando Dodino <fernando.dodino@gmail.com>
2022-04-03 19:19:00 -0300

    Solution

--- src/main/kotlin/ar/edu/unsam/algo2/contenidos/Contenidos.kt
+++ src/main/kotlin/ar/edu/unsam/algo2/contenidos/Contenidos.kt
@@ -1,2 +1,22 @@
 package ar.edu.unsam.algo2.contenidos
 
+import java.time.LocalDate
+
+abstract class Contenido(val presupuesto: Int) {
+  // Template method
+  fun esInteresante() = presupuesto > 5 && cumpleCriterioInteresante()
+
+  // Primitive
+  abstract fun cumpleCriterioInteresante(): Boolean
+}
+
+class Serie(presupuesto: Int, val temporadas: Int = 5) : Contenido(presupuesto) {
+  // Primitive
+  override fun cumpleCriterioInteresante() = temporadas < 5
+}
+
+class Pelicula(presupuesto: Int, val fechaEstreno: LocalDate = LocalDate.now()) : Contenido(presupuesto) {
+  // Primitive
+  override fun cumpleCriterioInteresante() = seEstrenoRecientemente()
+  fun seEstrenoRecientemente() = fechaEstreno.year == LocalDate.now().year
+}
--- src/test/kotlin/ar/edu/unsam/algo2/contenidos/PeliculasSpec.kt
+++ src/test/kotlin/ar/edu/unsam/algo2/contenidos/PeliculasSpec.kt
@@ -7,26 +7,26 @@ import java.time.LocalDate
 
 class PeliculasSpec : DescribeSpec({
   isolationMode = IsolationMode.InstancePerTest
-//  describe("Test películas") {
-//    it("película con presupuesto bajo no es interesante") {
-//      // Arrange
-//      val peliculaSinPresupuesto = Pelicula(presupuesto = 2)
-//      // Assert
-//      peliculaSinPresupuesto.esInteresante() shouldBe false
-//    }
-//    it("película estrenada hace mucho no es interesante") {
-//      // Arrange
-//      val hace2Anios = LocalDate.now().minusYears(2)
-//      val peliculaVieja = Pelicula(presupuesto = 6, fechaEstreno = hace2Anios)
-//      // Assert
-//      peliculaVieja.esInteresante() shouldBe false
-//    }
-//    it("película estrenada recientemente y con buen presupuesto es interesante") {
-//      // Arrange
-//      val peliculaEstrenada = Pelicula(presupuesto = 6, temporadas = 4)
-//      // Assert
-//      peliculaEstrenada.esInteresante() shouldBe true
-//    }
-//  }
+  describe("Test películas") {
+    it("película con presupuesto bajo no es interesante") {
+      // Arrange
+      val peliculaSinPresupuesto = Pelicula(presupuesto = 2)
+      // Assert
+      peliculaSinPresupuesto.esInteresante() shouldBe false
+    }
+    it("película estrenada hace mucho no es interesante") {
+      // Arrange
+      val hace2Anios = LocalDate.now().minusYears(2)
+      val peliculaVieja = Pelicula(presupuesto = 6, fechaEstreno = hace2Anios)
+      // Assert
+      peliculaVieja.esInteresante() shouldBe false
+    }
+    it("película estrenada recientemente y con buen presupuesto es interesante") {
+      // Arrange
+      val peliculaEstrenada = Pelicula(presupuesto = 6, fechaEstreno = LocalDate.now())
+      // Assert
+      peliculaEstrenada.esInteresante() shouldBe true
+    }
+  }
 })
 
